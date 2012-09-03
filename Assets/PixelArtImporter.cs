43f3a458f6316a50e044c2040e7684734528bcfe
joonas <jusjoo@gmail.com>
2012-09-03 18:02:39 +0300

    importtaus toimii, kuvan koordinaatistossa (0,0) on kuvan vasen alalaita

--- Assets/PixelArtImporter.cs
+++ Assets/PixelArtImporter.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PixelArtImporter : MonoBehaviour {
 	
 	public Vector2 sizeInPixels;
-	public string imageURL;
+	public Texture2D image;
 	
 	
 	private float cubeSize = 0.1f;
@@ -12,25 +12,31 @@ public class PixelArtImporter : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		
-		WWW www = new WWW(imageURL);
-		while (!www.isDone) {}
-		Texture2D image = www.texture;
-		
 		for (int x=0; x < sizeInPixels.x; x++) {
 			
 			for (int y=0; y < sizeInPixels.y; y++)  {
 				
-				Color color = image.GetPixel(x,y);
+				Color color = image.GetPixel(x, y);
+                Debug.Log(color);
 				
-				GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-				
-				cube.transform.position = new Vector3(x*cubeSize, y*cubeSize, 0);
+				GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+               
+                cube.transform.localScale = new Vector3(cubeSize, cubeSize, cubeSize);
+                
+                cube.transform.parent = this.transform;
+
+                cube.renderer.material.color = color;
+
+				cube.transform.position = new Vector3(this.transform.position.x + x*cubeSize, this.transform.position.y + y*cubeSize, 0);
+                
 				
 			}
 			
 		}
 		
 	}
+
+
 	
 	// Update is called once per frame
 	void Update () {
